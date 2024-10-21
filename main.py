9213c9fa87a4cb0310abb48d475ea637d12731f3
rogerboroda <roger_boroda@mail.ru>
2024-10-22 00:48:55 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,80 @@
+class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return object.__new__(cls)
+
+    def __init__(self, name, floors):
+        self.name = name
+        self.number_of_floors = floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):  # - должен возвращать строку: "Название: <название>, кол-во этажей: <этажи>"
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+    def go_to(self, new_floor):
+        if 1 <= new_floor <= self.number_of_floors:
+            for i in range(1, new_floor + 1):
+                print(i)
+        else:
+            print('"Такого этажа не существует"')
+
+    def __add__(self, other):
+        if isinstance(other, House):
+            self.number_of_floors += other.number_of_floors
+        elif isinstance(other, int):
+            self.number_of_floors += other
+        return self
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __iadd__(self, other):
+        return self.__add__(other)
+
+    def __eq__(self, other):
+        return self.number_of_floors == other.number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def __del__(self):
+        print(f"{self.name} снесен,но он останется в истории.")
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
+
+# Вывод:
+# ['ЖК Эльбрус']
+# ['ЖК Эльбрус', 'ЖК Акация']
+# ['ЖК Эльбрус', 'ЖК Акация', 'ЖК Матрёшки']
+# ЖК Акация снесён, но он останется в истории
+# ЖК Матрёшки снесён, но он останется в истории
+# ['ЖК Эльбрус', 'ЖК Акация', 'ЖК Матрёшки']
+# ЖК Эльбрус снесён, но он останется в истории
